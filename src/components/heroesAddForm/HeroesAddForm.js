5f9486035f529817c1cc631a760c6cd36f0d787a
YuliiaPaziura <yuliiapaziura@gmail.com>
2022-09-27 14:43:43 +0300

    Create new hero

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -1,3 +1,9 @@
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {v4 as uuidv4} from 'uuid';
+import { useHttp } from '../../hooks/http.hook';
+
+import { heroesFetched, heroesFetchingError } from '../../actions';
 
 
 // Задача для этого компонента:
@@ -11,11 +17,51 @@
 // данных из фильтров
 
 const HeroesAddForm = () => {
+    const [name, setName] = useState('');
+    const [descr, setDescr] = useState('');
+    const [element, setElement] = useState();
+
+    const {request} = useHttp();
+
+
+    useEffect(()=> {
+        console.log(element)
+
+    }, [element])
+
+
+
+    const {heroes} = useSelector(state => state);
+    const dispatch = useDispatch();
+
+    const addNewHero = (e, name, descr) => {
+        e.preventDefault();
+        
+        const newId = uuidv4();
+        const newHero = ({
+            "id": newId,
+            "name": name,
+            "description": descr,
+            "element": element
+        })
+
+        request(`http://localhost:3001/heroes/`, "POST", JSON.stringify(newHero))
+        .then(()=> dispatch(heroesFetched([...heroes, newHero])))
+        .catch(()=> dispatch(heroesFetchingError()))
+
+        setName('');
+        setDescr('');
+        setElement();
+    }
+
+
     return (
         <form className="border p-4 shadow-lg rounded">
             <div className="mb-3">
                 <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
                 <input 
+                    onInput={(e)=>setName(e.target.value)}
+                    value={name}
                     required
                     type="text" 
                     name="name" 
@@ -27,6 +73,8 @@ const HeroesAddForm = () => {
             <div className="mb-3">
                 <label htmlFor="text" className="form-label fs-4">Описание</label>
                 <textarea
+                    onInput={(e)=>setDescr(e.target.value)}
+                    value={descr}
                     required
                     name="text" 
                     className="form-control" 
@@ -38,6 +86,8 @@ const HeroesAddForm = () => {
             <div className="mb-3">
                 <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
                 <select 
+                    onChange={(e)=>setElement(e.target.value)}
+                    value={element}
                     required
                     className="form-select" 
                     id="element" 
@@ -50,7 +100,7 @@ const HeroesAddForm = () => {
                 </select>
             </div>
 
-            <button type="submit" className="btn btn-primary">Создать</button>
+            <button onClick={(e)=>{addNewHero(e, name, descr)}} type="submit" className="btn btn-primary">Создать</button>
         </form>
     )
 }
