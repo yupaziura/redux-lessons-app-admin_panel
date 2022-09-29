ce6ee9ade7c4f5067b281f10fa19a1d7a6311ced
YuliiaPaziura <yuliiapaziura@gmail.com>
2022-09-29 13:59:01 +0300

    Enchance store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,43 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
+
+
+// усиление стора
+
+// this function will change the work proncipals of store
+
+// bu if we want to change ony dispatch
+// we will use a middleware
+const enchencer = (createStore)=>(...args) => {
+    const store = createStore(...args);
+
+    const oldDispatch = store.dispatch;
+    store.dispatch = (action) => {
+        if (typeof action === 'string') {
+            return oldDispatch({
+                type:action
+            })
+        }
+        else oldDispatch(action)
+    }
+    return store;
+}
 
 const store = createStore(
     // use combine reducers to combine them
     // pass object in it
     combineReducers({heroes, filters})
-    , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+    , 
+    // we want our extension for google work too
+    // but we have to pass code as a 2d argument too
+    // so we can use a compose
+    compose(
+        enchencer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+    );
 
 export default store;
+
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
